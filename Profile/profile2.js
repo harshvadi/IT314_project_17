8a172e372b987cf090c86c77298db229b9447ff7
harshvadi <harshvadi12345@gmail.com>
2023-03-19 13:32:08 +0530

    another profile

---
+++ Profile/profile2.js
@@ -0,0 +1,14 @@
+function profile() {
+  document.getElementsByClassName("current")[0].classList.remove("current");
+  document.getElementsByClassName("profile")[0].classList.add("current");
+}
+
+function surveys() {
+  document.getElementsByClassName("current")[0].classList.remove("current");
+  document.getElementsByClassName("surveys")[0].classList.add("current");
+}
+
+function contributions() {
+  document.getElementsByClassName("current")[0].classList.remove("current");
+  document.getElementsByClassName("contributions")[0].classList.add("current");
+}
