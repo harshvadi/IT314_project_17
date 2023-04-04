2f1843f76a92a24e0978d8494ea2584e92c343f0
harshvadi <harshvadi12345@gmail.com>
2023-04-04 15:03:22 +0530

    added session expiry message

--- Profile/profile2.js
+++ Profile/profile2.js
@@ -13,19 +13,27 @@ function contributions() {
   document.getElementsByClassName("contributions")[0].classList.add("current");
 }
 
-window.onload = (event) => {
+window.onload = async (event) => {
   const name = document.getElementById("name");
   const username = document.getElementById("username");
   const profile = document.getElementById("profile-pic");
   const bio = document.getElementById("bio");
 
   let user = localStorage.getItem("user");
-  user = JSON.parse(user);
+  user = await JSON.parse(user);
 
-  name.innerText = user.name;
-  username.innerText = user.username;
-  bio.innerText = user.bio;
-  profile.src = user.profilepic;
+  if (user) {
+    name.innerText = user.name;
+    username.innerText = user.username;
+    bio.innerText = user.bio;
+    profile.src = user.profilepic;
+    document.getElementsByClassName("avatar")[0].src = user.profilepic;
+  } else {
+    document.body.innerHTML = `
+      <div class="container-fluid align-self-center">
+        <div class="h1 text-center">Session Expired. Please <a style="text-decoration:underline" href="../Auth/signin-signup.html">LogIn</a> again.</div>
+      </div>`;
+  }
 
   console.log("user:", user);
 };
