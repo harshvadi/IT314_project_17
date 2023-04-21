e9313145e321ef0c9f608ce39d6638556f25b384
Harsh Prajapati <harshmp492@gmail.com>
2023-04-21 18:10:06 +0530

    feed page added and integrated

--- Feed/feed.js
+++ Feed/feed.js
@@ -34,29 +34,20 @@ const limitthevisibilitychar = (count, id) => {
 
 
 limitthevisibitywords(6, "trending-poll-name");
-limitthevisibilitychar(15, "suggest-user1");
-limitthevisibilitychar(15, "suggest-user2");
-limitthevisibilitychar(15, "suggest-user3");
-limitthevisibilitychar(15, "suggest-user4");
-limitthevisibilitychar(15, "suggest-user5");
+// limitthevisibilitychar(15, "suggest-user1");
+// limitthevisibilitychar(15, "suggest-user2");
+// limitthevisibilitychar(15, "suggest-user3");
+// limitthevisibilitychar(15, "suggest-user4");
+// limitthevisibilitychar(15, "suggest-user5");
 
-const clickopenresponse1 = document.getElementById("open-poll-response-page1");
-const clickopenresponse2 = document.getElementById("open-poll-response-page2");
-const clickopenresponse3 = document.getElementById("open-poll-response-page3");
-const clickopenresponse4 = document.getElementById("open-poll-response-page4");
-const clickopenresponse5 = document.getElementById("open-poll-response-page5");
-const clickopenresponse6 = document.getElementById("open-poll-response-page6");
+const clickopenresponse1 = document.getElementById("open-poll-response-page");
 
-const openPollResponsePage = () => {
-  window.location.href = "../poll.html";
-}
 
-clickopenresponse1.addEventListener("click", openPollResponsePage);
-clickopenresponse2.addEventListener("click", openPollResponsePage);
-clickopenresponse3.addEventListener("click", openPollResponsePage);
-clickopenresponse4.addEventListener("click", openPollResponsePage);
-clickopenresponse5.addEventListener("click", openPollResponsePage);
-// clickopenresponse6.addEventListener("click", openPollResponsePage);
+const redirecttohostpoll = document.getElementsByClassName('submit-button-setting');
+
+// const redirecttohostpoll.addEventListener('click', () => {
+
+// });
 
 const options_list = document.querySelectorAll("label");
 for (let i = 0; i < options_list.length; i++) {
@@ -98,3 +89,210 @@ for (let i = 0; i < options_list.length; i++) {
 }
 
 
+/****** API integrations babyyy */
+
+// on reaching the bottom of the page, load more polls
+
+const getmorepolls = async () => {
+  const token = localStorage.getItem("token");
+  if(!token) return;
+  const response = await fetch("http://localhost:3000/api/feed/0",{
+    method: "POST",
+    headers: {
+      Accept: "applicaiton/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      'token': token,
+    }),
+    withCredentials: true, // should be there
+    credentials: 'include' // should be there 
+
+  });
+  const data = await response.json();
+  return data;
+};
+
+const loadMorePolls = async () => {
+  let pagecount = localStorage.getItem("pagecount") | 0;
+  let feeditems = await getmorepolls();
+  if(!feeditems){
+    alert('No more polls to load');
+    return;
+  }
+  feeditems = feeditems.feedItems;
+  console.log(feeditems);
+  for(let i=0;i<feeditems.length;i++){
+        const users_name = feeditems[i].name;
+        const users_username = feeditems[i].username;
+        const profileimg = feeditems[i].profilepic;
+        const questiontitle = feeditems[i].pollTitle;
+        const question = feeditems[i].question;
+        const questiondesc = feeditems[i].pollDescription;
+        const totalquestions = feeditems[i].totalquestions;
+        const totalresponses = feeditems[i].totalresponses;
+        const endedAt = feeditems[i].endedAt;
+        const hostedpoll_link = `http://localhost:5500/poll.html/`;
+        let iscontributedisabled = "none";
+        if(feeditems[i].questionType == "multiple"){
+            iscontributedisabled = "block";
+        }
+
+        if(iscontributedisabled == "block"){
+            const newitemcontainer = `
+            <div class="center-feed-poll-item" id=${pagecount*10+i}>
+            <div class="feed-center-title-bar-1">
+                <div class="feed-center-title-bar">
+                    <img src=${profileimg} class="user-profile-img-1" alt="profileimg">
+                    <div class="feed-center-title-user-info">
+                        <p id="feed-user-info-name">${users_name}</p>
+                        <p id="feed-user-info-username">@${users_username}</p>
+                    </div>
+                </div>
+                <div class="submit-btn-contain">
+                    <a class="submit-button-setting" target="_blank" href="../poll.html" id="open-poll-response-page${pagecount*10+i}" style="display:${iscontributedisabled}">Contribute</a>
+                </div>
+            </div>
+            <hr/>
+            <div class="feed-center-poll-question">
+                <p id="question-title-center-feed">${(question && question.length > 0) ? question : questiontitle}</p>
+                <p class="poll-question-desc">
+                    ${questiondesc}
+                </p>
+                <div class="side-info-question-feed">
+                    <div>
+                        <span>Questions : <span>${totalquestions}</span></span>
+                    </div>
+                    <div>
+                        <span>Responses : <span>${totalresponses}</span></span>
+                    </div>
+                    <div>
+                        <span>Due : <span>${endedAt}</span></span>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </div>
+            `
+            // console.log("item");
+            const feed = document.getElementById("feedismine");
+            feed.innerHTML += newitemcontainer;
+          }
+          else{
+
+            // generate labels for options
+            let options = feeditems[i].options;
+            console.log(options)
+            let optionshtml = "";
+            let inputhtml = "";
+            for(let j=0;j<options.length;j++){
+              const option = options[j];
+              inputhtml += `
+                <input type="checkbox" name="poll-option" id="poll-option-${pagecount*10 + i + j}" class="">
+              `;
+                optionshtml += `
+                          <label for="opt-${pagecount*10 + i +j}" class="opt-${pagecount*10 + i +j}">
+                            <div class="row">
+                              <div class="column">
+                                <span class="circle"></span>
+                                <span class="text">${options[j]}</span>
+                              </div>
+                              <span class="percent">30%</span>
+                            </div>
+                            <div class="progress" style='--w:30;'></div>
+                          </label>
+                `;
+            }
+
+            const users_name = feeditems[i].name;
+          const users_username = feeditems[i].username;
+          const profileimg = feeditems[i].profilepic;
+          const questiontitle = feeditems[i].pollTitle;
+          const question = feeditems[i].question;
+          const questiondesc = feeditems[i].pollDescription;
+
+
+            const newitemcontainer = `
+            <div class="center-feed-poll-item">
+                <div class="feed-center-title-bar-1">
+                    <div class="feed-center-title-bar">
+                        <img src=${profileimg} class="user-profile-img-1" alt="profileimg">
+                        <div class="feed-center-title-user-info">
+                            <p id="feed-user-info-name">${users_name}</p>
+                            <p id="feed-user-info-username">@${users_username}</p>
+                        </div>
+                    </div>
+                    <div class="submit-btn-contain">
+                        <!-- <button class="submit-button-setting" id="open-poll-response-page6">Contribute</button> -->
+                    </div>
+                </div>
+                <hr/>
+
+                <div class="feed-center-poll-question">
+                    <p id="question-title-center-feed">${question}</p>
+                    <div class="poll-center-options">
+                        ${inputhtml}
+                        ${optionshtml}
+                    </div>
+                </div>
+
+            </div>
+            `
+            console.log(newitemcontainer)
+            const feed = document.getElementById("feedismine");
+            feed.innerHTML += newitemcontainer;
+          }
+
+
+}
+
+  if (scrollY > scrollable) {
+    console.log("load more polls");
+  }
+}
+
+// wait 5sec before loading more polls
+
+loadMorePolls();
+
+// window.addEventListener("scroll", loadMorePolls);
+
+window.onload = function () {
+  const user = localStorage.getItem("user");
+  if (user) {
+
+    // const "{"_id":"642cb294f2c39fd0f2543008","username":"harshmp","email":"harshlove@gmail.com","role":"Customer","name":"Harsh Prajapati","bio":"Just started surveying.","profilepic":"https://www.w3schools.com/w3images/avatar2.png","__v":0}"
+    const userobj = JSON.parse(user);
+    const profileimg = userobj.profilepic;
+    const username = userobj.username;
+    const name = userobj.name;
+    const bio = userobj.bio;
+    const email = userobj.email;
+
+
+    let obj = `<div class="left-top-color-grid">
+                
+    </div>
+    <div class="left-top-container-bar">
+        <img src=${profileimg} class="over-the-bar-img left-top-container-bar-img" alt="profileimg">
+        <div class="content-box">
+            <p style="font-size: 18px; font-weight: 700">${name}</p>
+            <p class="shadow-color" style="font-size: 14px;">@${username}</p>
+            <hr>
+            <p>Followings</p>
+            <p style="font-size: 14px;">142</p>
+            <hr>
+            <p>Followers</p>
+            <p style="font-size: 14px;">432</p>
+            <hr>
+            <a class="like-p" href="/" style="cursor:pointer;">View Profile</a>
+        </div>
+    </div>`
+
+    
+    const lefttop = document.getElementsByClassName("left-top-container")[0];
+    lefttop.innerHTML = obj;
+
+  }
+}
