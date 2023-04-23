39c57a02959c4f946d55e83023eddedf5230ff35
harshvadi07 <harshvadi12345@gmail.com>
2023-04-23 15:41:29 +0530

    integrated

---
+++ User Polls/userPolls.js
@@ -0,0 +1,81 @@
+async function getAllpolls() {
+  const token = JSON.parse(localStorage.getItem("token"));
+  const username = JSON.parse(localStorage.getItem("user")).username;
+  const response = await fetch(`${BACKEND_BASE_URL}/api/getallpollsbyuser`, {
+    method: "POST",
+    headers: {
+      Accept: "applicaiton/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: token,
+      username: username,
+    }),
+    withCredentials: true, // should be there
+    credentials: "include", // should be there
+  });
+  console.log(response.status);
+  const data = await response.json();
+
+  const polls = data.polls;
+
+  if (polls.length == 0) {
+    document.getElementsByClassName(
+      "polls"
+    )[0].innerHTML = `<h1 class="text-center">No polls created.</h1>`;
+  }
+
+  let i = 1;
+  polls.forEach((poll) => {
+    // console.log(poll);
+    const pollResponses = getResponses(poll._id);
+    // console.log(pollResponses);
+
+    document.getElementsByClassName(
+      "polls"
+    )[0].innerHTML += `<div class="row text-center poll">
+    <div class="col">
+      ${i}. ${poll.title}
+    </div>
+    <div class="col">
+    //   ${pollResponses}
+    </div>
+  </div>
+
+  <hr>`;
+    i++;
+  });
+}
+
+window.onload = async () => {
+  getAllpolls();
+};
+
+async function getResponses(pollid) {
+  const token = JSON.parse(localStorage.getItem("token"));
+  const username = JSON.parse(localStorage.getItem("user")).username;
+  const response = await fetch(`${BACKEND_BASE_URL}/api/getdetailsaboutPoll`, {
+    method: "POST",
+    headers: {
+      Accept: "applicaiton/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: token,
+      pollid: pollid,
+    }),
+    withCredentials: true, // should be there
+    credentials: "include", // should be there
+  });
+  console.log(response.status);
+  const data = await response.json();
+  //   console.log(data);
+
+  if (response.status === 200) {
+    console.log(
+      data.pollanalysisobj[data.pollanalysisobj.length - 1].responses.length
+    );
+  } else {
+    console.log(0);
+  }
+}
