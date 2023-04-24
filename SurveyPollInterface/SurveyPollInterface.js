3c665258385cf6c450471dc9d337a422a82f21ec
Prashasti2801 <75677179+Prashasti2801@users.noreply.github.com>
2023-04-25 00:48:07 +0530

    Update SurveyPollInterface.js

--- SurveyPollInterface/SurveyPollInterface.js
+++ SurveyPollInterface/SurveyPollInterface.js
@@ -33,36 +33,24 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+});
 
-    let mainform = document.body.querySelector('.main-form')
-    let question = document.body.querySelector('.question');
 
-    $('.answer-type').change(function () {
-        const type = $(this).prop('value')
-        console.log(type)
-        console.log(this.parentElement.querySelector('.answer'))
 
-        let answerContent = ``;
-        // const div = document.createElement("div")
-        // const input = document.createElement("input");
-        // const answer = document.body.querySelector('.answer');
+let questionCount = 0; // Keep track of the number of questions added
 
-        if (type === "text-box") {
-            // console.log('text box');
-            answerContent = `<input type="text" class="answer-box" placeholder="Answer">`
-        }
+function addQuestion() {
+    questionCount++;
 
-        // $(this).parentElement.querySelector('.answer').innerHTML = answerContent;
+    const questionsContainer = document.getElementById('main-form');
 
-    });
+    const questionDiv = document.createElement('div');
+    questionDiv.className = 'question';
+    questionDiv.innerHTML = `
+        <input type="text" class="question-box" name="question${questionCount}" placeholder="Question">
 
-    $('#add-question').click(function () {
-        var questionContent = `  <div class="question">
-
-        <input type="text" class="question-box" placeholder="Question">
-
-        <select class="answer-type">
-            <option selected>--Select--</option>
+        <select class="answer-type" name="answerType${questionCount}" onchange="showAnswerOptions(this)">
+            <option value="null">--Select--</option>
             <option value="text-box">Text Box</option>
             <option value="check-box">Check Box</option>
             <option value="multiple-choice">Multiple Choice</option>
@@ -71,13 +59,110 @@ window.addEventListener('DOMContentLoaded', event => {
             <option value="time-box">Time</option>
         </select>
 
-        <i class="fa-solid fa-trash"></i>
+        <div class="answer" style="display: none;">
+            <div class="text-box" style="display: none;">
+                <input type="text" name="answerOptions${questionCount}" class="answer-box" placeholder="Answer">
+            </div>
 
-        <div class="answer"></div>
-        
-        </div>`
+            <div class="check-box" style="display: none;">
+                <input type="checkbox" id="checkbox-id" name="answerOptions${questionCount}">
+                <label for="checkbox-id"><input type="text" class="multiple-box" placeholder="Add Field"></label>
+                <button id="add-field-button">Add Field</button>
+                <button id="delete-field-button">Delete Field</button>
+            </div>
 
-        mainform.innerHTML += questionContent;
+            <div class="multiple-choice" style="display: none;">
+                <input type="radio" id="radio-id" name="answerOptions${questionCount}">
+                <label for="radio-id"><input type="text" class="radio-box" placeholder="Add Field"></label>
+                <button id="add-field-button">Add Field</button>
+                <button id="delete-field-button">Delete Field</button>
+            </div>
 
-    });
+            <div class="file-upload" style="display: none;">
+                <label for="file" class="file-input-text">Select a file to upload</label>
+                <input type="file" class="file-input" id="file" name="answerOptions${questionCount}">
+            </div>
+
+            <div class="date-box" style="display: none;">
+                <input type="date" class="input-date" id="date" name="answerOptions${questionCount}">
+            </div>
+
+            <div class="time-box" style="display: none;">
+                <input type="time" class="input-time" id="time" name="answerOptions${questionCount}">
+            </div>
+        </div>
+
+        <button type="button" class="delete-question" onclick="deleteQuestion(this) ">Delete</button>
+    `;
+
+    questionsContainer.appendChild(questionDiv);
+}
+
+function showAnswerOptions(select) {
+    const questionDiv = select.parentNode;
+    const answerOptionsDiv = questionDiv.querySelector('.answer');
+
+    answerOptionsDiv.querySelector('.text-box').style.display = 'none';
+    answerOptionsDiv.querySelector('.check-box').style.display = 'none';
+    answerOptionsDiv.querySelector('.multiple-choice').style.display = 'none';
+    answerOptionsDiv.querySelector('.file-upload').style.display = 'none';
+    answerOptionsDiv.querySelector('.date-box').style.display = 'none';
+    answerOptionsDiv.querySelector('.time-box').style.display = 'none';
+
+
+    if (select.value === 'null'){
+        answerOptionsDiv.style.display = 'none';
+    }
+
+    else if (select.value === 'text-box'){
+        answerOptionsDiv.style.display = 'block';
+        const ans = answerOptionsDiv.querySelector('.text-box');
+        ans.style.display = 'block';
+    }
+
+    else if (select.value === 'check-box'){
+        answerOptionsDiv.style.display = 'block';
+        const ans = answerOptionsDiv.querySelector('.check-box');
+        ans.style.display = 'block';
+    }
+
+    else if (select.value === 'multiple-choice'){
+        answerOptionsDiv.style.display = 'block';
+        const ans = answerOptionsDiv.querySelector('.multiple-choice');
+        ans.style.display = 'block';
+    }
+
+    else if (select.value === 'file-upload'){
+        answerOptionsDiv.style.display = 'block';
+        const ans = answerOptionsDiv.querySelector('.file-upload');
+        ans.style.display = 'block';
+    }
+
+    else if (select.value === 'date-box'){
+        answerOptionsDiv.style.display = 'block';
+        const ans = answerOptionsDiv.querySelector('.date-box');
+        ans.style.display = 'block';
+    }
+
+    else if (select.value === 'time-box'){
+        answerOptionsDiv.style.display = 'block';
+        const ans = answerOptionsDiv.querySelector('.time-box');
+        ans.style.display = 'block';
+    }
+
+    else {
+        answerOptionsDiv.style.display = 'none';
+    }
+
+}
+
+function deleteQuestion(button) {
+    const questionDiv = button.parentNode;
+    questionDiv.parentNode.removeChild(questionDiv);
+}
+
+document.getElementById('questionForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Handle form submission logic here
+    // Access the questions and their answer types and values using the form elements
 });
